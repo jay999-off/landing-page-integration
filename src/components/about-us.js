43302888b9f88e4f9c97eebf5fe51e7371ddbe27
KARAN VAGHELA <karanvaghela1045@gmail.com>
2025-08-08 13:52:12 +0530

    commit my about-us page

---
+++ src/components/about-us.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUs = () => {
+	return (
+		<div style={{ padding: '2rem', textAlign: 'center' }}>
+			<h1>About Us</h1>
+			<p>
+				Welcome to our company! We are dedicated to providing the best service possible.<br />
+				This is a simple About Us page for testing purposes.
+			</p>
+		</div>
+	);
+};
+
+export default AboutUs;
